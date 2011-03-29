eee7da1f3f6fd358b2e76edd94de0ed9cd7e59eb
Skilldrick <skilldrick@gmail.com>
2011-03-29 23:02:35 +0100

    Changed content to new presentation.

--- uploadsettings.py
+++ uploadsettings.py
@@ -1,7 +1,7 @@
 site = 'ftp.skilldrick.co.uk'
 webRoot = '/public_html'
 localDir = '.' #by default use current directory
-remoteDir = 'tmpl'
+remoteDir = 'so_pres'
 
 ignoreDirs = ['.git', 'fancybox', 'safeinc']
 ignoreFileSuffixes = ['.py', '.pyc', '~', '#', '.swp',
